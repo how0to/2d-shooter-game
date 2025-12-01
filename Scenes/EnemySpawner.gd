1fd34e33ce63436a276c4006594dd6cfff333fd2
how0to <backh4999@gmail.com>
2025-12-01 11:17:38 +0200

    Enemy spawns
    halo

--- Scenes/EnemySpawner.gd
+++ Scenes/EnemySpawner.gd
@@ -2,13 +2,14 @@ extends Node
 
 @export var SpawnDistance: float = 0.0      # How far off-screen to spawn
 @export var SpawnRate: float = 1.5            # Seconds between spawns
-@export var player_path: NodePath
+#@export var player_path: NodePath
 
 @onready var player: CharacterBody2D = $"../Player"
 var EnemyScene = preload("res://Scenes/enemy_1.tscn")
 
 func _ready():
-	player = get_node(player_path)
+	#player = get_node(player_path)
+	spawn_enemy()
 	spawn_timer()
 
 func spawn_timer():
@@ -21,11 +22,10 @@ func spawn_enemy():
 		return
 
 	# Reduce spawn rate but clamp minimum
-	if SpawnRate > 0.1:
-		SpawnRate *= 0.75
+	if SpawnRate > 0.001:
+		SpawnRate *= 0.99
 	else:
-		SpawnRate = 0.1
-
+		SpawnRate = 0.001
 	# Random angle around player (2D)
 	var angle = randf() * TAU
 	var offset = Vector2(
@@ -38,5 +38,4 @@ func spawn_enemy():
 
 	var enemy = EnemyScene.instantiate()
 	enemy.global_position = spawn_pos
-
 	get_tree().current_scene.add_child(enemy)
